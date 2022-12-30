01c90d44ed2bb3971edc2c59fb4f5ee38b95905c
Omer Bresinski <omerbres@Omers-MacBook-Pro.local>
2022-12-30 14:02:05 +0200

    some frontend magic

--- src/api/entities/tweets/useTweets.ts
+++ src/api/entities/tweets/useTweets.ts
@@ -1,8 +1,25 @@
 import { api } from "@/api/utils/axios";
 import { useQuery } from "@tanstack/react-query";
 
+interface TweetResponse {
+  data: Tweet[];
+  meta: Metadata;
+}
+interface Tweet {
+  edit_history_tweet_ids: string[];
+  id: string;
+  text: string;
+}
+
+interface Metadata {
+  newest_id: "1606973249446985729";
+  next_token: "7140dibdnow9c7btw423hxfo8pqk61lq60o0st3dxc7jk";
+  oldest_id: "1575867506773749760";
+  result_count: 10;
+}
+
 export const useTweets = () => {
-  return useQuery(["tweets"], async () => {
+  return useQuery<TweetResponse>(["tweets"], async () => {
     const response = await api.get("/tweets");
     return await response.data;
   });
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,10 +5,14 @@ export const Home = () => {
   const { data: loggedInUser } = useMe();
   const { data: tweets, error, isLoading } = useTweets();
 
-  console.log({ tweets, error, isLoading });
-
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+      }}
+    >
       <h1>Home</h1>
       {loggedInUser ? (
         <div>
@@ -22,6 +26,47 @@ export const Home = () => {
           </a>
         </form>
       )}
+      {error ? <h1>Error loading tweets</h1> : null}
+      {isLoading ? <h1>Loading tweets...</h1> : null}
+      {tweets ? (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "5px",
+          }}
+        >
+          <h2>Tweets</h2>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: "5px",
+              width: "100%",
+              height: "500px",
+              overflowY: "scroll",
+              border: "1px solid black",
+              padding: "10px",
+            }}
+          >
+            {tweets.data.map((tweet) => (
+              <div
+                style={{
+                  padding: "5px",
+                  border: "2px solid black",
+                  borderRadius: "5px",
+                  width: "250px",
+                  overflowWrap: "break-word",
+                }}
+                key={tweet.id}
+              >
+                {tweet.text}
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : null}
       <a href="http://127.0.0.1:4000/api/auth/logout">Log out</a>
     </div>
   );
