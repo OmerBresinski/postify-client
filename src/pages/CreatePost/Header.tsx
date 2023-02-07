daee8b2b30498de4bff24d63f65adaa85ec56b86
Omer Bresinski <omerbres@Omers-MacBook-Pro.local>
2023-02-07 13:17:45 +0200

    spacing fix

--- src/pages/CreatePost/Header.tsx
+++ src/pages/CreatePost/Header.tsx
@@ -5,7 +5,7 @@ import { Text } from "@/components/Text";
 export const Header = () => {
   return (
     <div className="flex w-full items-center justify-between gap-12  py-4 px-6">
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-[17px]">
         <Icon name="back" width={16} height={16} color="#404040" />
         <Text variant="header" color="gray500">
           New post
